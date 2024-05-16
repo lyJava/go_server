2dde79b4c357a003afe7d008c17ed0fc4c8431fa
菜哥 <745876299@qq.com>
2024-05-16 12:19:30 +0800

    ollama请求改动

--- api/expressAPI/controller/LlmsChatController.go
+++ api/expressAPI/controller/LlmsChatController.go
@@ -5,11 +5,14 @@ import (
 	"apiProject/api/utils"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
+	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 // LlmsController 大模型控制器
@@ -25,10 +28,12 @@ func LlmsControllerInit(llm *ollama.LLM) *LlmsController {
 }
 
 func (td *LlmsController) RegisterRoutes(router *mux.Router) {
-	router.HandleFunc("/llms/ollama", td.handleOllama3Ask).Methods("POST")
+	router.HandleFunc("/llms/ollama", td.handleOllama).Methods("POST")
+	router.HandleFunc("/llms/simple", td.handleSimple).Methods("POST")
+	router.HandleFunc("/llms/stream", td.handleStream).Methods("POST")
 }
 
-func (td *LlmsController) handleOllama3Ask(w http.ResponseWriter, r *http.Request) {
+func (td *LlmsController) handleOllama(w http.ResponseWriter, r *http.Request) {
 	var body utils.Chat
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
@@ -39,22 +44,12 @@ func (td *LlmsController) handleOllama3Ask(w http.ResponseWriter, r *http.Reques
 
 	// 使用了defer会在请求完成后处理关闭
 	defer utils.CloseBodyError("大模型问答请求body", w, r)
-	/*defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Printf("大模型问答请求body关闭错误===%v", err)
-			response.WriteJson(w, response.FailMessageResp("请求体关闭失败"))
-			return
-		}
-		log.Println("大模型问答请求body关闭成功")
-	}(r.Body)*/
-
 	log.Println("大模型问答请求body内容===", body)
 
 	prompt := utils.CreatePrompt()
 
 	data := map[string]any{
-		"text": body.Text,
+		"text": body.Prompt,
 	}
 
 	msg, _ := prompt.FormatMessages(data)
@@ -67,4 +62,100 @@ func (td *LlmsController) handleOllama3Ask(w http.ResponseWriter, r *http.Reques
 	log.Println("返回内容", content.Choices[0].Content)
 
 	response.WriteJson(w, response.OkDataResp(content))
+}
+
+func (td *LlmsController) handleSimple(w http.ResponseWriter, r *http.Request) {
+	var body utils.Chat
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		log.Printf("大模型问答参数解析失败===%v", err)
+		response.WriteJson(w, response.FailMessageResp("大模型问答请求失败"))
+		return
+	}
+
+	// 使用了defer会在请求完成后处理关闭
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("大模型问答请求body关闭错误===%v", err)
+			response.WriteJson(w, response.FailMessageResp("请求体关闭失败"))
+			return
+		}
+		log.Println("大模型问答请求body关闭成功")
+	}(r.Body)
+
+	log.Println("大模型问答请求body内容===", body)
+	utils.CreateSimplePrompt(td.Llm, w, body)
+}
+
+func (td *LlmsController) handleStream(w http.ResponseWriter, r *http.Request) {
+	var body utils.Chat
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		log.Printf("大模型问答参数解析失败===%v", err)
+		response.WriteJson(w, response.FailMessageResp("大模型问答请求失败"))
+		return
+	}
+
+	// 使用了defer会在请求完成后处理关闭
+	defer utils.CloseBodyError("大模型问答请求body", w, r)
+	log.Println("大模型问答请求body内容===", body)
+
+	prompt := utils.CreatePrompt()
+
+	data := map[string]any{
+		"text": body.Prompt,
+	}
+
+	msg, _ := prompt.FormatMessages(data)
+	messageContent := []llms.MessageContent{
+		llms.TextParts(msg[0].GetType(), msg[0].GetContent()),
+		llms.TextParts(msg[1].GetType(), msg[1].GetContent()),
+	}
+
+	ctx := context.Background()
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		response.WriteJson(w, response.FailMessageResp("Streaming not supported"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/x-ndjson")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = td.Llm.GenerateContent(ctx, messageContent, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		_, writeErr := w.Write(chunk)
+		fmt.Print(string(chunk))
+		if writeErr != nil {
+			return writeErr
+		}
+		flusher.Flush()
+		return nil
+	}))
+
+	if err != nil {
+		log.Printf("生成内容时出现错误==%v", err)
+		response.WriteJson(w, response.FailMessageResp("生成内容失败"))
+	}
+}
+
+func Read(resp *http.Response, out chan<- interface{}) error {
+	r := resp.Body
+	defer r.Close()
+	cr := httputil.NewChunkedReader(r)
+	for {
+		m := &http.Response{}
+		buf := make([]byte, 1024)
+		l, err := cr.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if pe := json.Unmarshal(buf[:l], m); pe != nil {
+			return pe
+		}
+		out <- m
+		if err == io.EOF {
+			return nil
+		}
+	}
 }
--- api/utils/cllama.go
+++ api/utils/cllama.go
@@ -1,13 +1,23 @@
 package utils
 
 import (
+	"apiProject/api/response"
+	"context"
+	"fmt"
+	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
 	"github.com/tmc/langchaingo/prompts"
 	"log"
+	"net/http"
 )
 
 type Chat struct {
-	Text string `json:"text"`
+	Prompt string `json:"prompt"`
+	Model  string `json:"model,omitempty"`  // 模型名称
+	Format string `json:"format,omitempty"` // 返回响应的格式。目前唯一接受的值是json
+	Stream bool   `json:"stream,omitempty"` // false响应是否作为单个响应对象返回，而不是对象流
+	Raw    bool   `json:"raw,omitempty"`    // true没有格式化，将应用于提示。raw如果您在 API 请求中指定完整模板化提示，则可以选择使用该参数
+	Images string `json:"images,omitempty"` // base64编码的图片
 }
 
 // CreateModel 创建大模型
@@ -27,4 +37,88 @@ func CreatePrompt() prompts.ChatPromptTemplate {
 			"text",
 		}),
 	})
+}
+
+// CreateSimplePrompt 创建请求简单模版
+func CreateSimplePrompt(llm *ollama.LLM, w http.ResponseWriter, content Chat) {
+	ctx := context.Background()
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		response.WriteJson(w, response.FailMessageResp("Streaming not supported"))
+		return
+	}
+
+	//w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	//w.WriteHeader(http.StatusOK)
+
+	_, err := llms.GenerateFromSinglePrompt(
+		ctx,
+		llm,
+		content.Prompt,
+		llms.WithTemperature(0.8),
+		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			fmt.Print(string(chunk))
+			_, writeErr := w.Write(chunk)
+			if writeErr != nil {
+				fmt.Printf("w.Write(chunk) error==%v", writeErr)
+				return writeErr
+			}
+			flusher.Flush()
+			return nil
+		}),
+	)
+
+	fmt.Print("\r\n")
+	if err != nil {
+		log.Printf("生成内容时出现错误==%v", err)
+		response.WriteJson(w, response.FailMessageResp("生成内容失败"))
+		return
+	}
+}
+
+// CreateSimplePromptChan generates text from a given prompt using the provided LLM.
+// It returns a channel from which the generated text can be read line by line.
+func CreateSimplePromptChan(llm *ollama.LLM, prompt string) (<-chan string, error) {
+	ctx := context.Background()
+	lineChan := make(chan string)
+
+	go func() {
+		defer close(lineChan) // Ensure the channel is closed when done
+
+		_, err := llms.GenerateFromSinglePrompt(
+			ctx,
+			llm,
+			prompt,
+			llms.WithTemperature(0.8),
+			llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+				line := string(chunk)
+				fmt.Print(line)
+				lineChan <- line
+				return nil
+			}),
+		)
+
+		if err != nil {
+			log.Printf("出现错误==%v", err)
+			lineChan <- fmt.Sprintf("error: %v", err)
+		}
+
+		fmt.Print("\r\n")
+	}()
+
+	return lineChan, nil
+}
+
+func PromptChan(ll *ollama.LLM, prompt string) {
+	lineChan, err := CreateSimplePromptChan(ll, prompt)
+	if err != nil {
+		log.Fatalf("Failed to create prompt: %v", err)
+	}
+
+	// Reading from the channel line by line
+	for line := range lineChan {
+		fmt.Println("Received:", line)
+		//time.Sleep(1 * time.Second) // Simulating processing delay
+	}
 }
