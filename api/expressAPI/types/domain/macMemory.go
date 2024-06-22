0af99799d04776b83998ad992a9a9f573f76e2cb
菜哥 <745876299@qq.com>
2024-06-22 13:05:17 +0800

    字段类型改动

--- api/expressAPI/types/domain/macMemory.go
+++ api/expressAPI/types/domain/macMemory.go
@@ -7,5 +7,5 @@ type MacMemory struct {
 	MemorySpeed     string `json:"memorySpeed,omitempty"`     // 内存频率(MHz)
 	IntegrationFlag string `json:"integrationFlag,omitempty"` // 是否集成
 	MemoryType      string `json:"memoryType,omitempty"`      // 内存类型
-	EccCheck        int32  `json:"eccCheck,omitempty"`        // 是否支持ECC校验
+	EccCheck        string `json:"eccCheck,omitempty"`        // 是否支持ECC校验
 }
