ba26130840dbdf748de458612230b2d02330dbb4
菜哥 <745876299@qq.com>
2025-03-07 17:17:55 +0800

    mysql连接初始化改动

--- api/expressAPI/datasource/mysql.go
+++ api/expressAPI/datasource/mysql.go
@@ -1,9 +1,9 @@
 package datasource
 
 import (
+	"apiProject/api/expressAPI/config"
+	cfg "apiProject/api/expressAPI/types/config"
 	"database/sql"
-	"log"
-
 	"github.com/go-sql-driver/mysql"
 	"github.com/google/wire"
 	"go.uber.org/zap"
@@ -14,20 +14,49 @@ type MysqlDB struct {
 }
 
 // InitMysqlDB 初始化mysql
-func InitMysqlDB(cfg mysql.Config) *MysqlDB {
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+func InitMysqlDB() *MysqlDB {
+	// 从配置文件中获取mysql的配置信息
+	viperConfig := config.ReadConfig("api/expressAPI/config", "application", "yml")
+	if viperConfig == nil {
+		zap.L().Sugar().Errorf("无mysql配置信息")
+		return nil
+	}
+
+	var sqlConfig cfg.SqlConfig
+	if err := viperConfig.Unmarshal(&sqlConfig); err != nil {
+		zap.L().Sugar().Errorf("获取mysql配置错误:%+v", err)
+		return nil
+	}
+
+	mysqlItem := sqlConfig.Mysql
+
+	// 构建mysql连接相关配置
+	buildConfig := mysql.Config{
+		User:                 mysqlItem.Username,
+		Passwd:               mysqlItem.Password,
+		Addr:                 mysqlItem.Url,
+		DBName:               mysqlItem.Database,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            false,
+		Collation:            "utf8mb4_general_ci",
+		Loc:                  config.EnvConfig.Loc,
+	}
+
+	db, err := sql.Open("mysql", buildConfig.FormatDSN())
 	if err != nil {
 		zap.L().Sugar().Errorf("mysql连接错误==%+v", err)
-		log.Fatalln(err)
+		//log.Fatalln(err) 这里会直接退出程序
+		return nil
 	}
 	err = db.Ping()
 	if err != nil {
 		zap.L().Sugar().Errorf("mysql-ping错误==%+v", err)
-		log.Fatal(err)
+		return nil
 	}
 	//db.SetMaxIdleConns(20)
 	//db.SetMaxOpenConns(10)
-	log.Println("Mysql connection initialized successfully")
+	zap.L().Sugar().Info("Mysql connection initialized successfully")
 	return &MysqlDB{Db: db}
 }
 
@@ -39,7 +68,6 @@ func (s *MysqlDB) GetDb() (*sql.DB, error) {
 
 	err := row.Scan(&version)
 	if err != nil {
-		log.Printf("查询Mysql数据库版本失败==%+v", err)
 		zap.L().Sugar().Errorf("查询Mysql数据库版本失败==%+v", err)
 		return nil, err
 	}
